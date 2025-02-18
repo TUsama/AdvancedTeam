389e074fbffdb57b73838c9e932abf028b735879
TUsama <1716976420@qq.com>
2025-02-18 17:40:07 +0800

    actually extend the show time of application

--- common/src/main/java/com/clefal/teams/client/gui/toast/ToastApplied.java
+++ common/src/main/java/com/clefal/teams/client/gui/toast/ToastApplied.java
@@ -1,5 +1,8 @@
 package com.clefal.teams.client.gui.toast;
 
+import com.clefal.teams.config.ATClientConfig;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.toasts.ToastComponent;
 import net.minecraft.network.chat.Component;
 
 import java.util.UUID;
@@ -13,6 +16,16 @@ public class ToastApplied extends RespondableTeamToast {
         super(team);
         this.name = name;
         this.id = id;
+    }
+
+    @Override
+    public Visibility render(GuiGraphics graphics, ToastComponent manager, long startTime) {
+        super.render(graphics, manager, startTime);
+
+        if (getResponded()) {
+            return Visibility.HIDE;
+        }
+        return startTime - getFirstDrawTime() < ATClientConfig.config.info.inviteToastShowSecond.get() * 1000L && team != null ? Visibility.SHOW : Visibility.HIDE;
     }
 
     public static ToastApplied of(String team, String name, UUID id){
